3217cc3c2a6918897d3c4300fc7b26597b911f54
Sree Charan <charsree@amazon.com>
2025-10-14 14:16:18 -0500

    Add MacPowerTools formula

---
+++ Formula/macpowertools.rb
@@ -0,0 +1,23 @@
+class Macpowertools < Formula
+  desc "Mac Power Tools - Text extraction and clipboard history"
+  homepage "https://github.com/charsree/MacPowerTools"
+  url "https://github.com/charsree/MacPowerTools/archive/refs/heads/main.tar.gz"
+  version "1.0.0"
+  sha256 "ded45f2310da3e9a36f00f4f3d120749086dba32bdd87c65c10697fa7590d35a"
+
+  depends_on :macos
+
+  def install
+    system "./build.sh"
+    bin.install "build/MacPowerTools"
+  end
+
+  def caveats
+    <<~EOS
+      To start MacPowerTools:
+        MacPowerTools &
+
+      Grant Screen Recording and Accessibility permissions when prompted.
+    EOS
+  end
+end
